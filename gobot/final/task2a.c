f5ac8aad18ff3023d1e76efe9a3fb38762a8422d
Advaith <advaith1@users.noreply.github.com>
2017-05-17 23:51:20 -0700

    Create task2a.c

---
+++ gobot/final/task2a.c
@@ -0,0 +1,24 @@
+#pragma config(Sensor, in1,    lineFollower,   sensorLineFollower)
+#pragma config(Sensor, in2,    potentiometer,  sensorPotentiometer)
+#pragma config(Sensor, dgtl1,  limitSwitch,    sensorTouch)
+#pragma config(Sensor, dgtl2,  bumpSwitch,     sensorTouch)
+#pragma config(Sensor, dgtl12, green,          sensorLEDtoVCC)
+#pragma config(Motor,  port1,           rightMotor,    tmotorVex393_HBridge, openLoop, reversed)
+#pragma config(Motor,  port2,           leftMotor,     tmotorServoContinuousRotation, openLoop)
+task main()
+{
+turnLEDOn(dgtl12);
+untilTouch(dgtl2);
+turnLEDOff(dgtl12);
+startMotor(port2,127);
+startMotor(port1,127);
+untilTouch(dgtl1);
+startMotor(port2,-127);
+startMotor(port1,-127);
+wait(1);
+startMotor(port2,127);
+startMotor(port1, -127);
+wait(4);
+stopMotor(port1);
+stopMotor(port2);
+}
